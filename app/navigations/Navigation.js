8d5ba72be776ebac589c432d5594f5c96d2a4252
Mauricio Cartagena <mc973220@gmail.com>
2020-04-27 23:50:28 -0400

    capitulo 1

---
+++ app/navigations/Navigation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Icon } from "react-native-elements";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import RestaurantsScreenStacks from "./RestaurantsStacks";
+
+const NavigationStacks = createBottomTabNavigator({
+  Restaurants: {
+    screen: RestaurantsScreenStacks,
+    navigationOptions: () => ({
+      tabBarLabel: "Restaurantes",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon
+          title="material-community"
+          name="compass-outline"
+          size={22}
+          color={tintColor}
+        />
+      ),
+    }),
+  },
+});
+export default createAppContainer(NavigationStacks);
